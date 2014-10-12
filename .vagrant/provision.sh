91fa126243716da2a3a41825d444e375d900d114
Peponi Futuro <mail@peponi.tk>
2014-10-12 22:41:37 +0200

    added vagrant, apache is not working with pelican

---
+++ .vagrant/provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo starting provision ...
+
+echo install python-pip
+
+apt-get install python-pip -y > /dev/null
+
+echo install project requirements
+
+cd /var/www
+pip install -r requirements.txt > /dev/null
+
+echo install apache2
+
+apt-get install apache2 > /dev/null
+
+cp .vagrant/configs/localhost /etc/apache2/sites-available/
+
+a2ensite localhost
+service apache2 reload
+
+#cd site/; ./develop_server.sh start 8080
---
+++ README.md
@@ -0,0 +1,9 @@
+# depencies
+
+[vagrant](http://www.vagrantup.com/) is requierd to initialize the development VM
+
+# installation
+
+``vagrant up``
+
+
