7f768ba4c4a0ddc601efe3e75b132d7d6c54b5f1
Peponi Futuro <mail@peponi.tk>
2014-11-30 20:57:28 +0100

    changed config files

--- .vagrant/provision.sh
+++ .vagrant/provision.sh
@@ -4,20 +4,22 @@ echo starting provision ...
 
 echo install python-pip
 
-apt-get install python-pip -y > /dev/null
+sudo apt-get install python-pip -y > /dev/null
 
 echo install project requirements
 
 cd /var/www
-pip install -r requirements.txt > /dev/null
+sudo pip install -r requirements.txt > /dev/null
 
-#echo install apache2
+echo install apache2
 
-#apt-get install apache2 > /dev/null
+sudo apt-get install apache2 > /dev/null
 
-#cp .vagrant/configs/localhost /etc/apache2/sites-available/
+sudo cp .vagrant/configs/localhost /etc/apache2/sites-available/
 
 #a2ensite localhost
-#service apache2 reload
+sudo service apache2 reload
 
-# cd /var/www/site/; ./develop_server.sh start 
+cd /var/www/site/
+
+sudo ./develop_server.sh start 
