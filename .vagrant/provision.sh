cdcc9af18df08c49ea5191994b69f0183940e67c
Danilo Bargen <mail@dbrgn.ch>
2015-02-17 23:10:54 +0100

    Simplified vagrant provisioning file

--- .vagrant/provision.sh
+++ .vagrant/provision.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
+echo Starting provisioning...
 
-echo starting provision ...
-
-echo install python-pip
-
+echo Installing python-pip
 apt-get install python-pip -y > /dev/null
 
-echo install project requirements
-
+echo Installing project requirements
 cd /var/www
-pip install -r requirements.txt > /dev/null
-
-#echo install apache2
-
-#apt-get install apache2 > /dev/null
-
-#cp .vagrant/configs/localhost /etc/apache2/sites-available/
-
-#a2ensite localhost
-#service apache2 reload
-
-# cd /var/www/site/; ./develop_server.sh start 
+pip install -q -U -r requirements.txt
